ecd1d2925291940783ca44068d67138dc32c56f1
Yevhenii <geniakravnew@gmail.com>
2026-06-06 21:25:37 +0300

    Add Accounts

---
+++ accounts/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from .serializers import RegisterSerializer, UserSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+
+class CurrentUserView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
