3f012d86ff5ee2d06dfd6dda4e61165c2da7b3b4
Yevhenii <geniakravnew@gmail.com>
2026-06-20 16:51:58 +0300

    Add Docker setup for Django and PostgreSQL

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Waiting for PostgreSQL..."
+
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
+  sleep 1
+done
+
+echo "PostgreSQL is ready."
+
+python manage.py migrate
+
+python manage.py runserver 0.0.0.0:8000
